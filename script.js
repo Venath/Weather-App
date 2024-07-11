8001f148bf744923687cb088e8988e38efeaf302
Venath <138109653+Venath@users.noreply.github.com>
2024-07-11 22:53:12 +0530

    images

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox=document.querySelector(".search input")
 const searchButton=document.querySelector(".search button")
+const weatherIcon=document.querySelector(".weatherIcon")
+
+console.log("object")
 
 async function checkWeather(city){
 
@@ -15,6 +18,20 @@ async function checkWeather(city){
     document.querySelector(".temp").innerHTML=Math.round(data.main.temp);
     document.querySelector(".humidity").innerHTML=data.main.humidity+" %";
 
+if(data.weather[0].main=="Clouds"){
+    weatherIcon.src="images/clouds.png"
+}
+else if(data.weather[0].main=="Clear"){
+    weatherIcon.src="images/clear.png"
+}
+else if(data.weather[0].main=="Mist"){
+    weatherIcon.src="images/mist.png"
+}
+else if(data.weather[0].main=="Snow"){
+    weatherIcon.src="images/snow.png"
+}
+
+
 }
 
 
@@ -23,5 +40,7 @@ searchButton.addEventListener("click", ()=>{
     console.log(city)
     checkWeather(city)
 })
+
+checkWeather("New York"); //default newyork
 
 
